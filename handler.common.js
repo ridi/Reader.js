5f656c1c973594ef5c4ddb9e9ab39563bf1fc159
kering <freekering@gmail.com>
2016-04-19 12:54:34 +0900

    findLink 동작 방식 더 원할하게 수정

--- handler.common.js
+++ handler.common.js
@@ -2,34 +2,21 @@
 
 var EventHandler = function() {};
 EventHandler.prototype = {
-    findAdjacentElements: function(/*Number*/x, /*Number*/y) {
-        var n = 4;
-        var points = [{x: x, y: y},
-                      {x: x - n, y: y - n},
-                      {x: x + n, y: y - n},
-                      {x: x - n, y: y + n},
-                      {x: x + n, y: y + n}];
-        var els = [];
-        for (var i = 0; i < points.length; i++) {
-            var point = points[i];
-            var el = doc.elementFromPoint(point.x, point.y);
-            if (el) {
-                els.push(el);
+    findLink: function(/*Number*/x, /*Number*/y) {
+        var tolerance = 4;
+        var point = adjustPoint(x, y);
+        for (x = point.x - tolerance; x <= point.x + tolerance; x++) {
+            for (y = point.y - tolerance; y <= point.y + tolerance; y++) {
+                var el = doc.elementFromPoint(x, y);
+                if (el) {
+                    var link = epub.getLinkFromElement(el);
+                    if (link !== null) {
+                        return link;
+                    }
+                }
             }
         }
-        return els;
-    },
-
-    findLink: function(/*Number*/x, /*Number*/y) {
-        var point = adjustPoint(x, y);
-        var link = null;
-        [].forEach.call(this.findAdjacentElements(point.x, point.y), function(el) {
-            link = epub.getLinkFromElement(el);
-            if (link !== null) {
-                return;
-            }
-        });
-        return link;
+        return null;
     },
 
     processSingleTapEvent: function() {
@@ -38,8 +25,7 @@ EventHandler.prototype = {
 
     processLongTapZoomEvent: function() {
         mustOverride('processLongTapZoomEvent');
-    },
-
+    }
 };
 
 var RidiEventHandler = function() {};
