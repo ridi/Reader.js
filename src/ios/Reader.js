c23ad653d9035b2c01f26f58279f9018501ea735
Davin Ahn <davin.ahn@ridi.com>
2020-09-06 20:52:38 +0900

    [iOS] 스크롤 이동은 네이티브에서 하도록 함

--- src/ios/Reader.js
+++ src/ios/Reader.js
@@ -27,25 +27,6 @@ export default class Reader extends _Reader {
   }
 
   /**
-   * @param {number} offset
-   */
-  scrollTo(offset = 0) {
-    // offset이 maxOffset을 넘길 수 없도록 보정한다. 이게 필요한 이유는 아래와 같다.
-    // - 보기 설정 미리보기를 보여주는 중에 마지막 페이지보다 뒤로 이동해 빈 페이지가 보이는 것을 방지
-    let adjustOffset = offset;
-    if (this.context.isScrollMode) {
-      const height = this.context.pageHeightUnit;
-      const maxOffset = this.totalHeight - height;
-      adjustOffset = Math.min(adjustOffset, maxOffset);
-    } else {
-      const width = this.context.pageWidthUnit;
-      const maxPage = Math.max(Math.ceil(this.totalWidth / width) - 1, 0);
-      adjustOffset = Math.min(adjustOffset, maxPage * width);
-    }
-    super.scrollTo(adjustOffset);
-  }
-
-  /**
    * @param {number} width
    * @param {number} height
    * @param {number} gap
@@ -53,8 +34,6 @@ export default class Reader extends _Reader {
    * @param {number} fontSize
    */
   changePageSizeWithStyle(width, height, gap, style, fontSize) {
-    const prevPage = this.curPage;
-
     this.context = Context.build((context) => {
       Object.assign(context, this.context);
       Object.assign(context, { width, height, gap });
@@ -66,7 +45,5 @@ export default class Reader extends _Reader {
 
     const wrapperStyle = this.getContent().ref.style;
     wrapperStyle['font-size'] = `${fontSize}%`;
-
-    this.scrollTo(prevPage * this.context.pageUnit);
   }
 }
